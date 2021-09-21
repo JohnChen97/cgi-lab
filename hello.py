1c774521612c2836aac6e7c7ed669c0c6b2e60aa
JohnChen97 <chenjionghao@yahoo.com>
2021-09-21 13:24:24 -0600

    add hello.py to repository

---
+++ hello.py
@@ -0,0 +1,11 @@
+import os, sys, json, socket
+#!/usr/bin/python
+print('Content-type:text/html\r\n\r\n')
+print('<html>')
+print('<head>')
+print('<title>Hello World</title>')
+print('</head>')
+print('<body>')
+print('<h2>Again, hello world</h2>')
+print('</body>')
+print('</html>')
